02c7a2e8576c633ecea66e8090d653bb543403c8
Ben <lakeconstance78@wolke7.net>
2022-07-27 12:59:25 +0200

    Update iptv_channels.bash
    fixing bug due to copyright breakdowns

--- iptv_channels.bash
+++ iptv_channels.bash
@@ -20,14 +20,16 @@
 #PUT YOUR M3U PLAYLISTS HERE
 #https://github.com/iptv-org/iptv
 M3U_PLAYLIST_1=https://iptv-org.github.io/iptv/index.country.m3u
-#https://github.com/Free-IPTV/Countries
-M3U_PLAYLIST_2=https://raw.githubusercontent.com/Free-IPTV/Countries/master/ZZ_PLAYLIST_ALL_TV.m3u
+
+#https://github.com/Free-IPTV/Countries -> Copyright Takedown
+#M3U_PLAYLIST_2=https://raw.githubusercontent.com/Free-IPTV/Countries/master/ZZ_PLAYLIST_ALL_TV.m3u
+
 #https://github.com/Free-TV/IPTV
-M3U_PLAYLIST_3=https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8
-#https://github.com/jnk22/kodinerds-iptv
-M3U_PLAYLIST_4=http://bit.ly/kn-clean
+M3U_PLAYLIST_2=https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8
+https://github.com/jnk22/kodinerds-iptv
+M3U_PLAYLIST_3=http://bit.ly/kn-clean
 #https://github.com/hayatiptv/iptv
-M3U_PLAYLIST_5=https://tinyurl.com/skaae8k
+M3U_PLAYLIST_4=https://tinyurl.com/skaae8k
 
 #TIMEOUT
 TIMEOUT=10.0
@@ -35,14 +37,14 @@ TIMEOUT=10.0
 rm iptv-channels -r
 
 echo "--------------------------------------------------------------------------------"
-git clone https://github.com/lakeconstance78/iptv-channels.git
+#git clone https://github.com/lakeconstance78/iptv-channels.git
 
 #DOWNLOAD PLAYLISTS
 wget $M3U_PLAYLIST_1 -O ./iptv-channels/M3U_PLAYLIST_1.m3u
 wget $M3U_PLAYLIST_2 -O ./iptv-channels/M3U_PLAYLIST_2.m3u
 wget $M3U_PLAYLIST_3 -O ./iptv-channels/M3U_PLAYLIST_3.m3u
 wget $M3U_PLAYLIST_4 -O ./iptv-channels/M3U_PLAYLIST_4.m3u
-wget $M3U_PLAYLIST_5 -O ./iptv-channels/M3U_PLAYLIST_5.m3u
+#wget $M3U_PLAYLIST_5 -O ./iptv-channels/M3U_PLAYLIST_5.m3u
 
 echo "--------------------------------------------------------------------------------"
 for file in ./iptv-channels/*.m3u
